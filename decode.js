02a8dfe80cdbce75d788f66f0491a6bbcf864556
jshubham895 <jshubham895@gmail.com>
2021-09-02 18:23:01 +0530

    final commit

--- decode.js
+++ decode.js
@@ -4,12 +4,18 @@ const fs = require("fs");
 const CryptoJS = require("crypto-js");
 const secretKey = fs.readFileSync(process.argv[4], "utf-8");
 
+console.log("Processing...");
+
 const str = fs.readFileSync(process.argv[2], "utf-8");
+
+console.log("Decrypting...");
+
 var bytes = CryptoJS.AES.decrypt(str, secretKey);
 var originalText = bytes.toString(CryptoJS.enc.Utf8);
 
 const buffer = Buffer.from(originalText, "base64");
 
 fs.writeFileSync(process.argv[3], buffer);
+fs.unlinkSync(process.argv[2]);
 
 console.log("File Decrypted Successfully !!");
--- encode.js
+++ encode.js
@@ -5,9 +5,17 @@ const CryptoJS = require("crypto-js");
 
 const secretKey = fs.readFileSync(process.argv[4], "utf-8");
 
+console.log("Processing...");
+
 const base64str = fs.readFileSync(process.argv[2], "base64");
+
 var ciphertext = CryptoJS.AES.encrypt(base64str, secretKey).toString();
+
+console.log("Encrypting...");
 
 fs.writeFileSync(process.argv[3], ciphertext, "utf-8");
 
-console.log("File Encrypted Successfully");
+console.log("File Encrypted Successfully.");
+console.log("Keep the secret key in order to decrypt the file.");
+
+fs.unlinkSync(process.argv[2]);
